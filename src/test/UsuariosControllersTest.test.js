66165b2b5859e35425294e5cfb28fc6c7b62fec8
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2023-11-14 17:02:20 -0500

    test

--- src/test/UsuariosControllersTest.test.js
+++ src/test/UsuariosControllersTest.test.js
@@ -1,7 +1,7 @@
 const { FindAllUser, CreateUser } = require("../repository/UserRepository");
 const { Response } = require("../utils/Response");
 const { findAll, create } = require("../controllers/UsuariosController");
-const { mockRequest, mockResponse } = require("../test/Mocks");
+const { mockRequest, mockResponse } = require("../test/Mocks/Mocks");
 
 jest.mock("../repository/UserRepository.js");
 
@@ -10,39 +10,23 @@ const returnUsers = {
   message: "Registros Encontrados",
   result: [
     {
-      _id: "651dc5e8016dc5b14c0bdda5",
-      password: "$2a$10$zRiVr9OjycUc/YcDWXF/4elHo7dJglBEAfrJKhfWQ/9rJ252KGP/W",
-      nombres: "stiven",
-      apellidos: "barajas",
-      email: "brayan@gmail.com",
-      usuario: "stiven",
-    },
-    {
-      _id: "6507ac64016dc5badc0bd4ad",
-      password: "$2a$10$qPeMksU6eH7952T1sChsBOEJSvNYGsoPl8YXCbq62WsUxix9GtOd2",
-      nombres: "juan",
-      apellidos: "quiroga",
-      email: "mq137267@gmail.com",
-      usuario: "quiroguin",
-    },
-    {
-      _id: "65369802016dc5360d0bde9d",
-      password: "$2a$10$YujMM0xWAx9ms16R9/3C1./bAfWW/YL1l3XTL6ySIsKhV5Jo4LwxG",
-      nombres: "juan",
-      apellidos: "quiroga",
-      email: "mq137267@gmail.co",
-      usuario: "juan",
-    },
+      _id: "6553ebad7213e1d73d330176",
+      password: "$2a$10$lRnFuE7033QiScsrAtAOFOnCXMbyRENe719QyBX20WLWvhwFmMhfa",
+      nombres: "Yura",
+      apellidos: "Torres",
+      email: "yura12@gmail.com",
+      usuario: "yury",
+    }
   ],
 };
 
 const requestUser = {
   body: {
-  password: "123",
-  nombres: "stiven",
-  apellidos: "barajas",
-  email: "brayan@gmail.com",
-  usuario: "stiven",
+  password: "1234",
+  nombres: "yura",
+  apellidos: "Torres",
+  email: "yura12@gmail.com",
+  usuario: "yury",
 }};
 describe("Test Users Controller", () => {
   beforeEach(() => {
@@ -60,11 +44,11 @@ describe("Test Users Controller", () => {
 
   it("should data to Controller Create Fail Users", async () => {
     let req = mockRequest();
-    req.params.password= "123";
-    req.params.nombres= "stiven";
-    req.params.apellidos= "barajas";
-    req.params.email= "brayan@gmail.com";
-    req.params.usuario= "stiven";
+    req.params.password= "1234";
+    req.params.nombres= "Yura";
+    req.params.apellidos= "Torres";
+    req.params.email= "yura12@gmail.com";
+    req.params.usuario= "yury";
 
     const res = mockResponse();
     Response.message = returnUsers.message;
@@ -79,12 +63,3 @@ describe("Test Users Controller", () => {
   });
 });
 
-// let req = mockRequest();
-//         req.params.fecha = '2023-02-02';
-//         const res = mockResponse();
-//         findAllCitas.mockReturnValueOnce({
-//             status: 200, message: 'fetch success all', result: [{idPaciente: '123456'}]
-//         })
-
-//         await CitasMedicasController.listAllCitasFecha(req, res);
-//         expect(res.status).toHaveBeenCalledWith(200);
